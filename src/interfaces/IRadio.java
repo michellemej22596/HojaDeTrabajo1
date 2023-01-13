5b6f847ab608d9f0fbbfa89482da3c641d6eb47a
Michelle Mejía Villela <97720056+michellemej22596@users.noreply.github.com>
2023-01-13 15:02:40 -0600

    MM - Interfaz

--- src/interfaces/IRadio.java
+++ src/interfaces/IRadio.java
@@ -1,14 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package interfaces;
 
 /**
  *
- * @author Davis
+ * @author Michelle
  */
 public interface IRadio {
-    
+	/***
+	 * Este metodo enciende / Apaga el dispositivo, recibe una variable booleana
+	 * que tiene el estado actual del dispositivo
+	 * @param el estado
+	 * @return mensaje de apagado o encendido
+	 */
+	public String changeStatusRadio(boolean status);
+	
+	/***
+	 * Este metodo cambia a la siguiente estacion.
+	 * @param indice actual en la lista
+	 * @return indice de la estacion
+	 */
+	public double nextStation(int index);
+	
+	/***
+	 * Este metodo cambia a la estacion anterior.
+	 * @param indice actual en la lista
+	 * @return indice de la estacion
+	 */
+	public double prevStation(int index);
+	
+	/***
+	 * Este metodo guarda la estaion FM
+	 * @param boton (numero del 1 al 6) donde se desea guardar la estacion
+	 * @return nombre de la estacion
+	 */
+	public String saveStationFm(int button);
+	
+	/***
+	 * Este metodo guarda la estaion AM
+	 * @param boton (numero del 1 al 6) donde se desea guardar la estacion
+	 * @return nombre de la estacion
+	 */
+	public String saveStationAm(int button);
+	
+	/***
+	 * Este metodo permite subir el volumen hasta 30
+	 * @return volumen
+	 */
+	public int upVolume();
+	
+	/***
+	 * Este metodo permite bajar el volumen hasta 0
+	 * @return volumen
+	 */
+	public int downVolume();
+	
 }
